f7fcaa1741f045fbe213e229c9bc8744a51c68a0
2Soon <2867727968@qq.com>
2017-12-23 16:02:21 +0800

    Update 1 A-B.java

--- Basic exercise/1 A-B.java
+++ Basic exercise/1 A-B.java
@@ -1,1 +1,13 @@
+package lanqiao;
 
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner ssc=new Scanner(System.in);
+		Integer a=ssc.nextInt();
+		Integer b=ssc.nextInt();
+		System.out.println(a+b);
+	}
+
+}

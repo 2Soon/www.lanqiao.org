999b3d73e86b6adb25729cbcc77811e96c098103
2Soon <2867727968@qq.com>
2018-03-03 17:28:52 +0800

    3
    333

--- 基础练习a/01字串 - 副本.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class Main{
-	public static void main(String[] args){
-		for(int i=0;i<32;i++){
-			String s="00000"+Integer.toBinaryString(i);
-			System.out.println(s.substring(s.length()-5));
-		}
-	}
-}

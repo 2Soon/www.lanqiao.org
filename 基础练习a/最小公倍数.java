9f4252538d781b90468e4decc0272ef8dbf4dda3
2Soon <2867727968@qq.com>
2018-03-31 10:22:16 +0800

    313
    3

--- 基础练习a/最小公倍数.java
+++ 基础练习a/最小公倍数.java
@@ -1,9 +1,24 @@
+package lq327;
 import java.util.Scanner;
-public class Main{
-	public static void main(String[] args){
-		Scanner sc=new Scanner(System.in);
-		int a=sc.nextInt();
-		int b=sc.nextInt();
-		
-	}
+public class gbs{  
+    public static void main(String[] args) {    
+        Scanner sc = new Scanner(System.in);    
+        int a = sc.nextInt();    
+        int b = sc.nextInt();    
+        sc.close();    
+        System.out.println(lcm(a, b));    
+    }    
+        
+    private static int lcm(int a, int b) {    
+        int g = gcd(a, b);    
+        return a * b / g;    
+    }    
+        
+    private static int gcd(int a, int b) {    
+        if (b == 0) {    
+            return a;    
+        } else {    
+            return gcd(b, a % b);  //辗转相除法  
+        }    
+    }    
 }
